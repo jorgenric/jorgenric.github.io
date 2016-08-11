ba92b5a470baf8aa69d42f47aacab6e2997c288a
Eric Jorgenson <jorgenric@gmail.com>
2016-08-11 13:56:30 -0700

    fleshed out local pw eval response code

--- pw-gen/services/checkPwService.js
+++ pw-gen/services/checkPwService.js
@@ -4,24 +4,8 @@
 (function(){
     
     var pwGenModule = angular.module("passwordGenerator");
-    
-    //
-    
+
     var checkPwService = function($http){
-        
-        var errorResponse = function(response){
-            return response.data || "Request Failed";
-        }
-        
-        var successResponse = function(response){
-            return response.data;
-        }
-        
-        var checkRemote = function(password){
-            var strength;
-            var apiPath = "https://www.google.com/accounts/RatePassword?Passwd=" + password;
-            return $http.post(apiPath);
-        }
         
         var getPwRankLocal = function(password){
             var passwordStr;
@@ -48,18 +32,35 @@
             var passwordStr = strengthRank || getPwRankLocal(password);
             var strengthMsg = "";
             if(passwordStr == 0){
+                strengthMsg = "Well, it's better than 'password'";
+            }if(passwordStr == 1){
                 strengthMsg = "Gotta say, pretty weak";
             }else if(passwordStr == 2){
                 strengthMsg = "Maybe you wont get hacked";
             }else if(passwordStr == 3){
                 strengthMsg = "Yeah, that's pretty good";
             }else if(passwordStr == 4){
-                strengthMsg = "Brute force will have to be quite brute";
+                strengthMsg = "Your password is a brick house, it's mighty mighty.";
             }else{
                 strengthMsg = "Hmmm, getting a weird value... not sure";   
             }
         
             return strengthMsg; 
+        }
+        
+        var errorResponse = function(response){
+            return response.data || "Request Failed";
+        }
+        
+        var successResponse = function(response){
+            return response.data;
+        }
+        
+        var checkRemote = function(password){
+            var strength;
+            // need to setup a server to complete this query do to cross origin restrictions, future state
+            var apiPath = "https://www.google.com/accounts/RatePassword?Passwd=" + password;
+            return $http.post(apiPath);
         }
         
         return {
